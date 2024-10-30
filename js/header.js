91c3c1f07582095c8de5a0b7ad842f0cb9bc5979
Rick <rick-2007@outlook.com>
2024-10-30 15:14:10 +0100

    minor header changes
    small

--- js/header.js
+++ js/header.js
@@ -5,11 +5,11 @@ input.addEventListener('input', () => {
   if (input.value.trim() !== '') {
     folderIcon.classList.remove('fa-folder-magnifying-glass');
     folderIcon.classList.add('fa-xmark');
-    folderIcon.setAttribute('title', 'Clear search field');
+    folderIcon.setAttribute('data-title', 'Clear search field');
   } else {
     folderIcon.classList.remove('fa-xmark');
     folderIcon.classList.add('fa-folder-magnifying-glass');
-    folderIcon.setAttribute('title', 'Browse');
+    folderIcon.setAttribute('data-title', 'Browse');
   }
 });
 
@@ -18,18 +18,22 @@ folderIcon.addEventListener('mousedown', (event) => {
     input.value = '';
     event.preventDefault();
     input.focus();
-    folderIcon.classList.remove('fa-xmark');
-    folderIcon.classList.add('fa-folder-magnifying-glass');
-    folderIcon.setAttribute('title', 'Browse');
+    setTimeout(() => {
+      folderIcon.classList.remove('fa-xmark');
+      folderIcon.classList.add('fa-folder-magnifying-glass');
+      folderIcon.setAttribute('data-title', 'Browse');
+    }, 150);
   }
 });
 
 folderIcon.addEventListener('click', () => {
-  if (folderIcon.classList.contains('fa-light')) {
-    folderIcon.classList.remove('fa-light');
-    folderIcon.classList.add('fa-solid');
-  } else {
-    folderIcon.classList.remove('fa-solid');
-    folderIcon.classList.add('fa-light');
+  if (!folderIcon.classList.contains('fa-xmark')) {
+    if (folderIcon.classList.contains('fa-light')) {
+      folderIcon.classList.remove('fa-light');
+      folderIcon.classList.add('fa-solid');
+    } else {
+      folderIcon.classList.remove('fa-solid');
+      folderIcon.classList.add('fa-light');
+    }
   }
 });
